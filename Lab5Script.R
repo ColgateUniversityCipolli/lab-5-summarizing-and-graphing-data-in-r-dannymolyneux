04cdda34d4fb67a5b2b4913f5ca1ed129bbec0a4
dannymolyneux <dmolyneux@colgate.edu>
2025-02-24 00:28:29 -0500

    Step 2
    Got the function to work for all features

--- Lab5Script.R
+++ Lab5Script.R
@@ -5,28 +5,30 @@ essentia.data = read.csv("data/essentia.data.csv") #data for all the tracks othe
 allentown.data = read.csv("data/essentia.data.allentown.csv")  #data for allentown
 #view(allentown.data)
 
-#function that determines whether Allentown is out of range, unusual, or within the range for each band
-check_range = function(data){
-  data |>
+#function that determines whether a given feature in Allentown is out of range, unusual, or within the range for each band
+check_range = function(feature){
+  essentia.stats = essentia.data |> #defining stats for each feature, grouped by artist
     group_by(artist) |>
-    summarize(min = min(overall_loudness), LF = median(overall_loudness) - 2*IQR(overall_loudness), UF = median(overall_loudness) + 2*IQR(overall_loudness), max = max(overall_loudness)) |>
-    mutate(out.of.range = (allentown.data$overall_loudness > max | allentown.data$overall_loudness < min)) |>
-    mutate(unusual = (allentown.data$overall_loudness > UF | allentown.data$overall_loudness < LF)) |>
-    mutate(description = case_when(out.of.range == TRUE ~ "Out of Range",
-                                   unusual == TRUE ~ "Outlying",
-                                   TRUE ~ "Within Range"))
+    summarize(min = min(get(feature), na.rm = TRUE), LF = median(get(feature), na.rm = TRUE) - 2*IQR(get(feature), na.rm = TRUE),
+              UF = median(get(feature), na.rm = TRUE) + 2*IQR(get(feature), na.rm = TRUE), 
+              max = max(get(feature), na.rm = TRUE))
+  essentia.stats |>
+    mutate(
+      out.of.range = (allentown.data[[feature]] > max | allentown.data[[feature]] < min),  #considered out of range
+      unusual = (allentown.data[[feature]] > UF | allentown.data[[feature]] < LF),  #considered unusual
+      description = case_when(out.of.range == TRUE ~ "Out of Range",  
+                              unusual == TRUE ~ "Outlying",
+                              TRUE ~ "Within Range")  #within range if not unusual or out of range
+    )
 }
 
+#step 2 (apply the function to all of the data)
+numeric.cols = select_if(essentia.data, is.numeric)  #list of only numeric columns in essentia.data
+range_results <- list()  #empty list that will store the results for each feature
 
-
-
-
-#names = colnames(essentia.data)
-#numeric.cols = select_if(essentia.data, is.numeric)
-#for(feature in colnames(numeric.cols)){
- # check_range(essentia.data, feature)
-#}
-  
+for(feature in colnames(numeric.cols)) {  #goes through every numeric column
+  range_results[[feature]] <- check_range(feature)  #adds the info for that feature to the results list
+}
 
 
 
